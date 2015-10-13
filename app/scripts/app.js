a645ad44c7f45f51ea5f4349228c779a27bcc0a5
Uuganbayar Sukhbaatar <ubs121@gmail.com>
2015-10-13 19:16:44 +0800

    flip card

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,24 +1,18 @@
 /*
-Copyright (c) 2015 The Polymer Project Authors. All rights reserved.
-This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
-The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
-The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
-Code distributed by Google as part of the polymer project is also
-subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
+Copyright (c) 2015 ubs121
 */
 
 (function(document) {
   'use strict';
 
+  // өгөгдлийн сан
+  var ds = new DataService();
 
 
   // Grab a reference to our auto-binding template
   // and give it some initial binding values
   // Learn more about auto-binding templates at http://goo.gl/Dx1u2g
   var app = document.querySelector('#app');
-
-  var db = new Db();
-  app.db = db;
 
   app.displayInstalledToast = function() {
     // Check to make sure caching is actually enabled—it won't be in the dev environment.
--- app/scripts/db.js
+++ app/scripts/db.js
@@ -1,9 +1,9 @@
-var Db = function() {
-  // Following member variables are initialized within getDbConnection().
+var DataService = function() {
+  // Following member variables are initialized within getConnection().
   this.db_ = null;
-  window.ls = this;
-
-  console.log('Database created.');
+  this.deck_ = null;
+  this.card_ = null;
+  window.fs = this;
 };
 
 
@@ -12,7 +12,13 @@ var Db = function() {
  * connection to the database.
  * @private
  */
-Db.prototype.onConnected_ = function() {
+DataService.prototype.onConnected_ = function() {
+  this.deck_ = this.db_.getSchema().table('Deck');
+  this.card_ = this.db_.getSchema().table('Card');
+
+  this.loadDemo();
+
+
 };
 
 
@@ -20,9 +26,9 @@ Db.prototype.onConnected_ = function() {
  * Instantiates the DB connection (re-entrant).
  * @return {!IThenable<!lf.Database>}
  */
-Db.prototype.getConnection = function() {
+DataService.prototype.getConnection = function() {
   if (this.db_ != null) {
-    return this.db_;
+    return new Promise(function(resolve, reject) { resolve(this.db_); } );
   }
 
   var connectOptions = {storeType: lf.schema.DataStoreType.INDEXED_DB};
@@ -40,23 +46,79 @@ Db.prototype.getConnection = function() {
  * @return {!lf.schema.Builder}
  * @private
  */
-Db.prototype.buildSchema_ = function() {
+DataService.prototype.buildSchema_ = function() {
   var schemaBuilder = lf.schema.create('flashcard', 1);
 
-schemaBuilder.createTable('Deck').
-    addColumn('id', lf.Type.INTEGER).
-    addColumn('name', lf.Type.STRING).
-    addColumn('created', lf.Type.DATE_TIME).
-    addPrimaryKey(['id']);
+  schemaBuilder.createTable('Deck').
+      addColumn('id', lf.Type.INTEGER).
+      addColumn('name', lf.Type.STRING).
+      addColumn('created', lf.Type.DATE_TIME).
+      addPrimaryKey(['id']);
 
-schemaBuilder.createTable('Card').
-    addColumn('id', lf.Type.INTEGER).
-    addColumn('question', lf.Type.STRING).
-    addColumn('answer', lf.Type.STRING).
-    addColumn('interval', lf.Type.DOUBLE).
-    addPrimaryKey(['id']).
-    addIndex('idxInterval', ['interval'], false, lf.Order.DESC);
+  schemaBuilder.createTable('Card').
+      addColumn('id', lf.Type.INTEGER).
+      addColumn('question', lf.Type.STRING).
+      addColumn('answer', lf.Type.STRING).
+      addColumn('interval', lf.Type.NUMBER).
+      addColumn('created', lf.Type.DATE_TIME).
+      addPrimaryKey(['id']).
+      addIndex('idxInterval', ['interval'], false, lf.Order.DESC);
 
 
   return schemaBuilder;
 };
+
+
+// data insert
+DataService.prototype.importDecks = function(rs) {
+  var deckRows = rs.map(
+          function(obj) { obj.created = new Date();  return this.deck_.createRow(obj); }, this);
+
+  return this.db_.insertOrReplace().into(this.deck_).values(deckRows).exec();
+};
+
+
+DataService.prototype.importCards = function(rs) {
+  var cardRows = rs.map(
+          function(obj) { obj.created = new Date();  obj.interval = 1; return this.card_.createRow(obj); }, this);
+
+  return this.db_.insertOrReplace().into(this.card_).values(cardRows).exec();
+};
+
+DataService.prototype.getJson = function(url, onResponse) {
+  var xmlhttp = new XMLHttpRequest();
+
+  xmlhttp.onreadystatechange = function() {
+      if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+          var arr = JSON.parse(xmlhttp.responseText);
+          onResponse(arr);
+      }
+  }
+  xmlhttp.open("GET", url, true);
+  xmlhttp.send();
+};
+
+// demo data
+DataService.prototype.loadDemo = function() {
+  var row = this.deck_.createRow({
+    'id': 3,
+    'name': 'Capitals',
+    'created': new Date()
+  });
+
+  this.db_.insertOrReplace().into(this.deck_).values([row]).exec();
+
+  this.getJson("data/Deck.json", function(resp) {
+    console.log('Decks: ' + resp.length);
+    console.log(this.importDecks(resp));
+  }.bind(this));
+
+  this.getJson("data/Card.json", function(resp) {
+    console.log('Cards: ' + resp.length);
+    try {
+      this.importCards(resp);
+    } catch(e) {
+      // skip
+    }
+  }.bind(this));
+}
